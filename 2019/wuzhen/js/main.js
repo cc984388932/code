9ce9b86e470eb36dafb3ba2ef34b84a329fe8cd0
bigRubbish <984388932@qq.com>
2020-03-10 23:49:19 +0800

    项目重构

--- 2019/wuzhen/js/main.js
+++
@@ -1,8 +0,0 @@
-function resize() {
-    var clientWidth = document.documentElement.clientWidth;
-    clientWidth = clientWidth >= 640 ? 640 : clientWidth;	
-    /*  设计图做成750px的  */
-    document.documentElement.style.fontSize = 100 * (clientWidth / 750) + 'px';
-}
-resize();
-window.addEventListener('resize', resize);
